e18bf22fa3767b476c4eeadab279618d90c8831e
jorge <jorge.ramirez.br@gmail.com>
2023-05-29 01:24:42 +0200

    Tema 2, nivel 1, ex2

---
+++ PHP_Basic_Tema2/Nivell_1/ex_2.php
@@ -0,0 +1,11 @@
+<?php
+$hola = "Hello, World!";
+
+echo $hola . "<br>";
+echo strtoupper($hola) . "<br>";
+echo strlen($hola) . "<br>";
+echo strrev($hola) . "<br>";
+
+$curso = "Este es el curso de PHP";
+echo $hola . " ". $curso;
+?>
