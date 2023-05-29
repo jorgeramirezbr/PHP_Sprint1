39677fa028301eacfe815582d32a4784bb3d608d
jorge <jorge.ramirez.br@gmail.com>
2023-05-29 04:40:41 +0200

    Tema 3, nivel 1, ex4

---
+++ Arrays_Tema3/Nivell_1/ex_4.php
@@ -0,0 +1,15 @@
+<?php
+
+$info = [
+    "nombre" => "Jorge",
+    "edad" => 41,
+    "email" => "jorge.ramirez.br@gmail.com",
+    "comida_favorita" => "Ceviche"
+];
+
+/*Prueba
+echo "<pre>";
+var_dump($info);
+*/
+
+?>
