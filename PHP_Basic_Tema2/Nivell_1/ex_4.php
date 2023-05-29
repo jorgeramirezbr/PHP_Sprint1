d7d015e99ab7a5eade1114f3ddc2d95599b86601
jorge <jorge.ramirez.br@gmail.com>
2023-05-29 02:33:40 +0200

    Tema 2, nivel 1, ex4

---
+++ PHP_Basic_Tema2/Nivell_1/ex_4.php
@@ -0,0 +1,15 @@
+<?php
+
+function contar($numero = 10, $incremento = 1) {
+    for ($i = 1; $i <= $numero; $i += $incremento) {
+      echo $i . "<br>";
+    }
+  }
+  
+  /* Prueba de la funcion
+  
+  contar(20, 2);
+
+  */
+
+?>
